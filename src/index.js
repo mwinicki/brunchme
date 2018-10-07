4c0325b1a2cedc28ae320a5b9750863b3ee9615f
Marilu Winicki <mwinicki@gmail.com>
2018-10-07 13:32:38 -0500

    fixed express path to client

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ let restaurants = require ('./models/restaurantModel');
 let restaurantRoutes = require ("./routes/RestaurantRoutes");
 let commentRoutes = require ("./routes/commentRoutes");
 const app = express();
+const path = require('path');
 
     
 mongoose.connect("mongodb://user1:iamuser1@ds137812.mlab.com:37812/brunchme");
 
-
+app.use(express.static(path.join(__dirname, '../client')));
+console.log(path.join(__dirname, '../client'));
 app.use(bodyParser.json());
 app.use(restaurantRoutes);
 app.use(commentRoutes);
